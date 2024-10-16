f70157cb58747790d4bcb8d183e70aa561112be3
andriiklykavkaa <andriiklykavkaa@gmail.com>
2024-10-16 10:16:32 +0300

    Displayed Favorites in FavoritesListVC

---
+++ GHFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteCell.swift
+//  GHFollowers
+//
+//  Created by Andrii Klykavka on 15.10.2024.
+//
+
+import Foundation
---
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceManager.swift
+//  GHFollowers
+//
+//  Created by Andrii Klykavka on 15.10.2024.
+//
+
+import Foundation
